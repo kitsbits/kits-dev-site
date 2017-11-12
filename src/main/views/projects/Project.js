3a5ec5f559ee5e759a6d6df10d64d9a1a9b295f5
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-11-11 18:21:08 -0700

    fix project margins on mobile

--- src/main/views/projects/Project.js
+++ src/main/views/projects/Project.js
@@ -5,13 +5,20 @@ import Header from "./components/Header";
 import Photo from "./components/Photo";
 
 export default function Project(props) {
+    const mediaQueries = {
+        small: "@media screen and (max-width: 991px)"
+    }
+
     const Container = glamorous.div({
         margin: "300px auto 0 auto",
         width: "90%",
         backgroundColor: "white",
         borderRadius: "2px",
         boxShadow: "rgba(0, 0, 0, 0.4) 0px 1px 3px inset",
-        padding: "25px 55px"
+        padding: "25px 55px",
+        [mediaQueries.small]: {
+            marginTop: "200px"
+        }
     });
 
     ////// PHOTOS \\\\\\\
