df5be431bbb301fb93970dfdf009eb53ee49d655
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-11-27 11:38:39 -0500

    new projects page styling, fixed horizontal scroll

--- src/main/views/projects/Project.js
+++ src/main/views/projects/Project.js
@@ -15,14 +15,15 @@ export default function Project(props) {
 
     ////// STYLING \\\\\\\
     const mediaQueries = {
-        small: "@media screen and (max-width: 991px)"
+        small: "@media screen and (max-width: 991px)",
+        xsmall: "@media screen and (max-width: 700px)"
     }
 
     const ProjectContainer = glamorous.div({
         background: `url(${backgroundImg}) no-repeat`,
         paddingTop: "500px",
         width: "100wh",
-        height: "2500",
+        height: "auto",
         backgroundSize: "cover",
         paddingBottom: "25px",
     });
@@ -31,19 +32,46 @@ export default function Project(props) {
         width: "0",
         height: "0",
         borderStyle: "solid",
-        borderWidth: "0 3000px 900px 3000px",
+        borderWidth: "0 2500px 900px 2500px",
         borderColor: "transparent transparent white transparent",
-        position: "absolute",
-        transform: "translateX(-50%)",
+        overflow: "hidden",
+    });
+
+    const TopPics = glamorous.div({
+        height: "600px",
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        marginTop: "-150px",
+        [mediaQueries.small]: {
+            marginTop: "50px",
+            height: "auto",
+        }
+    });
+
+    const BottomPics = glamorous.div({
+        height: "400px",
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        marginTop: "50px",
+        [mediaQueries.small]: {
+            marginTop: "auto",
+            height: "auto",
+        }
     });
 
     const JournalPic = glamorous.div({
-        width: "488px",
-        height: "372px",
+        width: "547px",
+        height: "417px",
         background: `url(${journal}) no-repeat center`,
         backgroundSize: "cover",
         position: "relative",
         zIndex: "1",
+        alignSelf: "flex-end",
+        [mediaQueries.small]: {
+
+        }
     });
 
     const CalendarPic = glamorous.div({
@@ -61,19 +89,42 @@ export default function Project(props) {
         background: `url(${calculator}) no-repeat center`,
         backgroundSize: "cover",
         zIndex: "1",
+        position: "relative",
     });
 
     const ShitlistPic = glamorous.div({
-        width: "490px",
-        height: "319px",
+        width: "636px",
+        height: "414px",
         background: `url(${shitlist}) no-repeat center`,
         backgroundSize: "cover",
         zIndex: "1",
+        position: "relative",
     });
+
+    const WithLanguagesContainer = glamorous.div({
+        display: "flex",
+        flexDirection: "column",
+        alignSelf: "flex-end",
+    });
+
+    const Languages = glamorous.p({
+        color: "#FF4057",
+        fontWeight: "600",
+        zIndex: "1",
+        position: "relative",
+        fontSize: "1.5em",
+        width: "300px",
+        marginLeft: "-50px",
+        backgroundColor: "white",
+        padding: "10px",
+        [mediaQueries.small]: {
+            marginLeft: "0",
+        }
+    })
 
     return (
         <ProjectContainer id="project-container">
-            <Triangle/>
+            <div style={{position: "absolute", transform: "translateX(-50%)"}}><Triangle/></div>
             <Header
                 title={props.title}
                 description={props.description}
@@ -81,11 +132,17 @@ export default function Project(props) {
                 goText={props.goText}
                 seeCode={props.seeCode}
                 seeText={props.seeText}/>
-            <JournalPic/>
-            <CalendarPic/>
-            <CalculatorPic/>
-            <ShitlistPic/>
-
+            <TopPics>
+                <JournalPic/>
+                <WithLanguagesContainer>
+                    <CalendarPic/>
+                    <Languages>REACT + REDUX + EXPRESS + MONGODB + MONGOOSE</Languages>
+                </WithLanguagesContainer>
+            </TopPics>
+            <BottomPics>
+                <CalculatorPic/>
+                <ShitlistPic/>
+            </BottomPics>
         </ProjectContainer>
     )
 }
