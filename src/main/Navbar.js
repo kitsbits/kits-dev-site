6d6aa6c47509aa717396f3583ee590a54d620506
Kit Masaracchia <kit.masaracchia@gmail.com>
2017-12-05 20:14:22 -0500

    add scroll to state

--- src/main/Navbar.js
+++ src/main/Navbar.js
@@ -2,216 +2,98 @@ import React from "react";
 import { Link } from "react-router-dom";
 import glamorous from "glamorous";
 
-export default function Navbar(props) {
-    ///// STYLING \\\\\
-    const mediaQueries = {
-        small: "@media screen and (max-width: 991px)"
+export default class Navbar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            scroll: false
+        };
     }
 
-    // const Container = glamorous.div({
-    //     display: "flex",
-    //     flexWrap: "wrap",
-    //     justifyContent: "space-between",
-    //     alignItems: "center",
-    //     position: "fixed",
-    //     backgroundColor: "white",
-    //     width: "100vw",
-    //     height: "200px",
-    //     transition: "height 1s",
-    //     zIndex: "2",
-    //     [mediaQueries.small]: {
-    //         padding: "0",
-    //         height: "475px",
-    //         position: "static",
-    //     }
-    // });
+    render() {
+        ///// NAVBAR SCROLL FUNCTION \\\\\
+        window.addEventListener("scroll", () => {
+            const scrollTop = document.documentElement.scrollTop;
+            const windowWidth = window.innerWidth;
 
-    // const HiContainer = glamorous.div({
-    //     width: "320px",
-    //     padding: "15px",
-    //     fontSize: "1.35em",
-    //     alignSelf: "flex-start",
-    //     [mediaQueries.small]: {
-    //         position: "absolute",
-    //         top: "155px",
-    //         width: "100%",
-    //         textAlign: "center",
-    //     }
-    // });
+            let navContainer = document.getElementById("nav-container");
+            let hiContainer = document.getElementById("hi-container");
+            let kit = document.getElementById("kit");
+            let currentPageContainer = document.getElementById("current-page-container");
+            let linkContainer = document.getElementById("link-container");
 
-    // const HiThere = glamorous.h1({
-    //     margin: "0",
-    //     fontWeight: "300",
-    // });
-    //
-    // const ImKit = glamorous.h1({
-    //     color: "#FF4057",
-    //     margin: "0"
-    // });
-    //
-    // const Kit = glamorous.h1({
-    //     color: "#FF4057",
-    //     margin: "0",
-    //     fontSize: "3em",
-    //     opacity: "0.0",
-    //     marginLeft: "35px",
-    //     transition: "opacity 1.2s",
-    //     position: "absolute",
-    //     display: "hidden",
-    // });
+            if (scrollTop > 100 && windowWidth > 991) {
+                // document.getElementById("nav-container").style.height = "150px";
+                // document.getElementById("nav-container").style.overflow = "hidden";
+                // document.getElementById("nav-container").style.borderBottom = "4px solid #FF4057";
+                // document.getElementById("hi-container").style.transform = "translateY(-110%)";
+                // document.getElementById("kit").style.opacity = "1";
+                // document.getElementById("kit").style.display = "inherit";
+                // document.getElementById("current-page-container").style.transform = "translateY(-5%)";
+                // document.getElementById("current-page-container").style.color = "#FF4057";
+                // document.getElementById("current-page-container").style.border = "4px solid transparent";
 
-    // const CurrentPageContainer = glamorous.div({
-    //     height: "250px",
-    //     width: "250px",
-    //     border: "4px solid #FF4057",
-    //     transform: "translateY(65%)",
-    //     borderRadius: "2px",
-    //     display: "flex",
-    //     justifyContent: "center",
-    //     alignItems: "flex-end",
-    //     color: "white",
-    //     padding: "50px",
-    //     transition: "all 1s",
-    //     zIndex: "2",
-    //     position: "relative",
-    //     [mediaQueries.small]: {
-    //         width: "auto",
-    //         height: "auto",
-    //         border: "none",
-    //         transform: "translateY(130%)",
-    //         fontSize: "1.2em",
-    //         marginBottom: "55px",
-    //         backgroundColor: "#FF4057",
-    //         width: "100%",
-    //         alignItems: "center",
-    //
-    //     }
-    // });
+                this.setState({
+                    scroll: true
+                });
 
-    // const LinkContainer = glamorous.div({
-    //     display: "flex",
-    //     alignItems: "flex-end",
-    //     width: "300px",
-    //     justifyContent: "space-around",
-    //     transition: "all 0.2s ease-in-out",
-    //     [mediaQueries.small]: {
-    //         position: "fixed",
-    //         width: "100%",
-    //         height: "150px",
-    //         paddingBottom: "25px",
-    //         // alignItems: "center",
-    //         top: "0",
-    //         backgroundColor: "white",
-    //         zIndex: "2",
-    //     }
-    // });
+            }
 
-    // const NavLink = glamorous.a({
-    //     paddingBottom: "15px",
-    //     borderBottom: "4px solid #FF4057",
-    //     textDecoration: "none",
-    //     margin: "0",
-    //     color: "black",
-    //     fontSize: "1.5em",
-    //     fontWeight: "600",
-    //     transition: "all 0.2s ease-in-out",
-    //     ":hover": {
-    //         color: "#FF4057",
-    //     }
-    // });
+            if (scrollTop < 100 && windowWidth > 991) {
+                document.getElementById("nav-container").style.height = "200px";
+                document.getElementById("nav-container").style.overflow = "visible";
+                document.getElementById("nav-container").style.borderBottom = "none";
+                document.getElementById("hi-container").style.transform = "translateY(0%)";
+                document.getElementById("kit").style.opacity = "0.0";
+                document.getElementById("kit").style.display = "hidden";
+                document.getElementById("current-page-container").style.transform = "translateY(65%)";
+                document.getElementById("current-page-container").style.color = "white";
+                document.getElementById("current-page-container").style.border = "4px solid #FF4057";
+                document.getElementById("current-page-container").style.height = "250px";
+            }
 
-    // const NavPage = glamorous.span({
-    //     transition: "all 0.1s ease-in",
-    //     ":hover": {
-    //         color: "#FF4057",
-    //     }
-    // })
+            if (scrollTop > 175 && windowWidth < 991) {
+                document.getElementById("link-container").style.height = "120px";
+                document.getElementById("current-page-container").style.transform = "translateY(65%)";
+                document.getElementById("current-page-container").style.position = "fixed";
+                document.getElementById("current-page-container").style.top = "50px";
+                document.getElementById("current-page-container").style.height = "100px";
+                document.getElementById("hi-container").style.display = "none";
 
-    // const routeNavStyles = {
-    //     paddingBottom: "15px",
-    //     borderBottom: "4px solid #FF4057",
-    //     textDecoration: "none",
-    //     margin: "0",
-    //     color: "black",
-    //     fontSize: "1.5em",
-    //     fontWeight: "600",
-    //     transition: "hover 0.1s ease-in",
-    // }
+            }
 
-    ///// NAVBAR SCROLL FUNCTION \\\\\
-    window.addEventListener("scroll", () => {
-        const scrollTop = document.documentElement.scrollTop;
-        const windowWidth = window.innerWidth;
-
-        const navContainer = document.getElementById("nav-container");
-        const hiContainer = document.getElementById("hi-container");
-        const kit = document.getElementById("kit");
-        const currentPageContainer = document.getElementById("current-page-container");
-        const linkContainer = document.getElementById("link-container");
-
-        if (scrollTop > 100 && windowWidth > 991) {
-            // document.getElementById("nav-container").style.height = "150px";
-            // document.getElementById("nav-container").style.overflow = "hidden";
-            // document.getElementById("nav-container").style.borderBottom = "4px solid #FF4057";
-            // document.getElementById("hi-container").style.transform = "translateY(-110%)";
-            // document.getElementById("kit").style.opacity = "1";
-            // document.getElementById("kit").style.display = "inherit";
-            // document.getElementById("current-page-container").style.transform = "translateY(-5%)";
-            // document.getElementById("current-page-container").style.color = "#FF4057";
-            // document.getElementById("current-page-container").style.border = "4px solid transparent";
-
+            if (scrollTop < 189 && windowWidth < 991) {
+                document.getElementById("link-container").style.height = "150px";
+                document.getElementById("current-page-container").style.height = "200px";
+                document.getElementById("current-page-container").style.position = "relative";
+                document.getElementById("hi-container").style.display = "flex";
+                document.getElementById("hi-container").style.flexDirection = "column";
+            }
+        });
+        let navClass = ["nav-container"];
+        if(this.state.addClass) {
+            navClass.push("nav-container-scroll");
         }
 
-        if (scrollTop < 100 && windowWidth > 991) {
-            document.getElementById("nav-container").style.height = "200px";
-            document.getElementById("nav-container").style.overflow = "visible";
-            document.getElementById("nav-container").style.borderBottom = "none";
-            document.getElementById("hi-container").style.transform = "translateY(0%)";
-            document.getElementById("kit").style.opacity = "0.0";
-            document.getElementById("kit").style.display = "hidden";
-            document.getElementById("current-page-container").style.transform = "translateY(65%)";
-            document.getElementById("current-page-container").style.color = "white";
-            document.getElementById("current-page-container").style.border = "4px solid #FF4057";
-            document.getElementById("current-page-container").style.height = "250px";
-        }
-
-        if (scrollTop > 175 && windowWidth < 991) {
-            document.getElementById("link-container").style.height = "120px";
-            document.getElementById("current-page-container").style.transform = "translateY(65%)";
-            document.getElementById("current-page-container").style.position = "fixed";
-            document.getElementById("current-page-container").style.top = "50px";
-            document.getElementById("current-page-container").style.height = "100px";
-            document.getElementById("hi-container").style.display = "none";
-
-        }
-
-        if (scrollTop < 189 && windowWidth < 991) {
-            document.getElementById("link-container").style.height = "150px";
-            document.getElementById("current-page-container").style.height = "200px";
-            document.getElementById("current-page-container").style.position = "relative";
-            document.getElementById("hi-container").style.display = "flex";
-            document.getElementById("hi-container").style.flexDirection = "column";
-        }
-    });
-
-    return (
-        <div className="nav-container" id="nav-container">
-            <div className="hi-container" id="hi-container">
-                <h1 className="hi-there">Hi, there.</h1>
-                <h1 className="im-kit">I'm Kit and I'm a web developer</h1>
+        return (
+            <div className={navClass} id="nav-container">
+                <div className="hi-container" id="hi-container">
+                    <h1 className="hi-there">Hi, there.</h1>
+                    <h1 className="im-kit">I'm Kit and I'm a web developer</h1>
+                </div>
+                    <h1 className="kit" id="kit">Kit</h1>
+                <div className="current-page-container" id="current-page-container">
+                    <h1>{window.location.pathname === "/resume" ? "RESUME" : "RECENT PROJECTS"}</h1>
+                </div>
+                <div className="link-container" id="link-container">
+                    {window.location.pathname === "/resume" ?
+                    <Link to="/" className="route-nav"><span className="nav-page">PRO<span style={{display: "block"}}>JECTS</span></span></Link> :
+                    <Link to="/resume" className="route-nav"><span className="nav-page">RES<span style={{display: "block"}}>UME</span></span></Link>
+                    }
+                    <a target="_tab" href="https://github.com/noblepaper"><i className="nav-link fa fa-3x fa-github"></i></a>
+                    <a target="_tab" href="https://www.linkedin.com/in/kitmasaracchia/"><i className="nav-link fa fa-3x fa-linkedin"></i></a>
+                </div>
             </div>
-                <h1 className="kit" id="kit">Kit</h1>
-            <div className="current-page-container" id="current-page-container">
-                <h1>{window.location.pathname === "/resume" ? "RESUME" : "RECENT PROJECTS"}</h1>
-            </div>
-            <div className="link-container" id="link-container">
-                {window.location.pathname === "/resume" ?
-                <Link to="/" className="route-nav"><span className="nav-page">PRO<span style={{display: "block"}}>JECTS</span></span></Link> :
-                <Link to="/resume" className="route-nav"><span className="nav-page">RES<span style={{display: "block"}}>UME</span></span></Link>
-                }
-                <a target="_tab" href="https://github.com/noblepaper"><i className="nav-link fa fa-3x fa-github"></i></a>
-                <a target="_tab" href="https://www.linkedin.com/in/kitmasaracchia/"><i className="nav-link fa fa-3x fa-linkedin"></i></a>
-            </div>
-        </div>
-    )
+        )
+    }
 }
